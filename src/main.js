f9dd5fab8cbe0c339359f4e61f2ff0e615410930
Carlos Fins <clockwerkz@gmail.com>
2025-02-15 16:24:21 -0800

    converting tailwind to reg css

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
-import { ColorButton } from "./color-button";
 
-const buttonColors = ['white', 'red', 'green'];
-
-const renderColorButtons = () => {
-  const overlayRef = document.querySelector('#screen-overlay');
-  const buttonContainer = document.querySelector('.button-container');
-  if (!overlayRef || !buttonContainer) return;
-  buttonColors.forEach(color => {
-    const btn = ColorButton(color, overlayRef);
-    buttonContainer.appendChild(btn);
-  });
-}
 
 addEventListener("DOMContentLoaded", ()=> {
   // renderColorButtons();
-
+  const screenOverlay = document.getElementById("screen-overlay");
+  screenOverlay.addEventListener("click", () => {
+    if (!document.fullscreenElement) return;
+    screenOverlay.style.display = 'none';
+    document.documentElement.style.overflow = 'scroll';
+    document.body.scroll = "yes";
+    document.removeEventListener('fullscreenchange', exitFullscreenHandler); 
+  })
 });
